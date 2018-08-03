5e34f90fe54ef6b47a21f22fb2060ff681150674
wuzhe <wz_sean@163.com>
2018-08-03 13:48:15 +0800

    readme modify

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # grand_vehicle_detection
 a ssd_moblienet vehicel detection systems base on Tensorflow
+
+This is instruction for training a vihicel detector using the Tensorflow Object Detection systems. I will list the steps in details as possible as I can, if any questions or errors please let me know. The output of the detector will look like the following:
+
+![](readimg/img1.png)
+
+## get vehicle datasets
+The datasets all are from '汽车之家' [website](https://www.autohome.com.cn), we use Web crawler to download the vehicle images including  their labels, and totally there are 48856 pictures and 764 classfications. but the label only with classfication is not enough for object detection, we need location information for each image.
+
+the downloaded images are following:
+![](readimg/img2.jpg)
+
+## get loaction information and write into xml
+In order to train a detector, we require a dataset of images, bounding boxes and classifications. my method is utilizing the ssd_mobilenet models pretrained on coco datasets which can find out car and truck appearing in the picture, and exchange location information to coordinates, finally write into xml. Run the following commands:
+
+``` bash
+python get_image_label.py \
+```
+
+There may be some codes need to modify, you could modify it according to your needs. In my example, we put tfrecord files in path `./slim/data`, which those tfrecord files don't include location information. 
+
+The generated xml file is like following: 
+![](readimg/xml.jpg)
+
+## write images and xmls into tfrecord
+After preparing your images and xmls, your `./data` directory should appear as follows:
+
+```lang-none
+- labels.txt
++ images/
+  + train/
+  + eval/
++ annotations/
+  - eval.txt
+  - train.txt
+  + xmls/
+    + train/
+    + eval/
+```
+
+and run the `create_tf_record.py`,  tfrecord files for training will be generated in path `./output`
+
+## Configuring the Object Detection Pipeline
+The model parameters, training parameters and eval parameters are all defined by a config file. more details you could consult the [Tensorflow Object Detection API](https://github.com/tensorflow/models/edit/master/research/object_detection). In our example, we 'usessd_mobilenet_v1' model and corresponding parameters has been set up in config flies, you could modify it according to your needs.
+
+## Starting Training Jobs
+Note: `run.sh` is a shell script, which help us starting training, evaluation and exporting at one step. 
+
+To start training, execute the following command from the directory:
+
+``` bash
+python ./object_detection/train.py --train_dir='./output/train' --pipeline_config_path='./super_ssd_mobilenet_v1_vehicle.config'
+```
+
+Usually, it takes long time for training, when the training ends up, `model.ckpt` file will be generated in path `./output/train`, and this file contain weights information of ssd_mobilenet model, which apply to next step
+
+## Exporting the Tensorflow Graph
+After your model has been trained, you should export it to a Tensorflow graph proto. The checkpoint will typically consist of three files:
+
+* `model.ckpt-${CHECKPOINT_NUMBER}.data-00000-of-00001`
+* `model.ckpt-${CHECKPOINT_NUMBER}.index`
+* `model.ckpt-${CHECKPOINT_NUMBER}.meta`
+
+After confirmation, run the following command:
+
+``` bash
+python ./object_detection/export_inference_graph.py 
+--input_type image_tensor 
+--pipeline_config_path './super_ssd_mobilenet_v1_vehicle.config'
+--trained_checkpoint_prefix './output/train/model.ckpt-$current'
+--output_directory './output/exported_graphs'
+```
+
+Afterwards, you should see a directory named exported_graphs containing the SavedModel and frozen graph like this: `frozen_inference_graph.pb`, and this pb file is neccessary for inference.
+
+## Inference image
+we have pb file now, and put `frozen_inference_graph.pb` in path `./input_dir`, then choose an image into `./input_dir`, don't forget modity the image name, and start inference by execute following command:
+
+``` bash
+python ./inference.py --output_dir='./output_dir' --dataset_dir='./input_dir'
+```
+
+Ok, it works, the following is detection result:
+
+![](readimg/img3.png)
+
+## Discussion
+Any doubts or errors, please contact me by Emails: wz_sean@163.com
--- inference.py
+++ inference.py
@@ -1,52 +1,68 @@
 import argparse
 import os
+import io
+import time
 
 import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
 from PIL import Image
 
-from utils import visualization_utils as vis_util
-from utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+from object_detection.utils import label_map_util
 
 if tf.__version__ < '1.4.0':
     raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!')
 
-NUM_CLASSES = 5
+NUM_CLASSES = 90
 
 
 def parse_args(check=True):
     parser = argparse.ArgumentParser()
     parser.add_argument('--output_dir', type=str, required=True)
-    parser.add_argument('--dataset_dir', type=str, required=True)
+    parser.add_argument('--input_dir', type=str, required=True)
     FLAGS, unparsed = parser.parse_known_args()
     return FLAGS, unparsed
 
+def load_label_to_list(label_path):
+    f = open(label_path, encoding="utf-8")
+    categories = []
+    category_line = {}
+    line = f.readline()
+    while line:
+        line = line.strip('\n')
+        line_info = line.split(':')
+        line_id = int(line_info[0])
+        line_name = line_info[1]
+        category_line = {'id': line_id, 'name': line_name}
+        categories.append(category_line)
+        line = f.readline()
+    f.close()
+    return categories
 
-if __name__ == '__main__':
-    FLAGS, unparsed = parse_args()
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
 
-    PATH_TO_CKPT = os.path.join(FLAGS.output_dir, 'exported_graphs/frozen_inference_graph.pb')
-    PATH_TO_LABELS = os.path.join(FLAGS.dataset_dir, 'labels_items.txt')
+def mainfunction(test_img_path):
+    # FLAGS, unparsed = parse_args()
+    # PATH_TO_CKPT = os.path.join('output_dir', 'exported_graphs/frozen_inference_graph.pb')
+    # PATH_TO_LABELS = os.path.join('input_dir', 'labels_items.txt')
 
     detection_graph = tf.Graph()
     with detection_graph.as_default():
         od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        with tf.gfile.GFile('input_dir/frozen_inference_graph_4.pb', 'rb') as fid:
             serialized_graph = fid.read()
             od_graph_def.ParseFromString(serialized_graph)
             tf.import_graph_def(od_graph_def, name='')
 
-    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    #读取pytxt为json
+    #label_map = label_map_util.load_labelmap('input_dir/mscoco_label_map.pbtxt')
+    #每一类作为字典放到list
+    #categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    categories = load_label_to_list('input_dir/labels.txt')
     category_index = label_map_util.create_category_index(categories)
-
-    def load_image_into_numpy_array(image):
-        (im_width, im_height) = image.size
-        return np.array(image.getdata()).reshape(
-            (im_height, im_width, 3)).astype(np.uint8)
-
-    test_img_path = os.path.join(FLAGS.dataset_dir, 'test.jpg')
 
     with detection_graph.as_default():
         with tf.Session(graph=detection_graph) as sess:
@@ -58,15 +74,89 @@ if __name__ == '__main__':
             image = Image.open(test_img_path)
             image_np = load_image_into_numpy_array(image)
             image_np_expanded = np.expand_dims(image_np, axis=0)
+            point1=time.time()
             (boxes, scores, classes, num) = sess.run(
                 [detection_boxes, detection_scores, detection_classes, num_detections],
                 feed_dict={image_tensor: image_np_expanded})
+            point2 = time.time()
+            print(point2 - point1)
+            #######################################################################
+            # 去掉size为1的维度
+            boxes_squeeze = np.squeeze(boxes)
+            scores_squeeze = np.squeeze(scores)
+            classes_squeeze = np.squeeze(classes).astype(np.int32)
+
+            keep = py_cpu_nms(boxes_squeeze, scores_squeeze, 0.5)
+            boxes_list = np.zeros((100, 4))
+            scores_list = np.zeros(100)
+            classes_list = np.zeros(100)
+            save_nums = len(keep)
+
+            for s in keep:
+                # list.index(i)
+                boxes_list[keep.index(s), :] = boxes_squeeze[s]
+                scores_list[keep.index(s)] = scores_squeeze[s]
+                classes_list[keep.index(s)] = classes_squeeze[s]
+
+            classes_list = classes_list.astype(np.int32)
+            #因为label编号的问题，所以这里要全部-1
+            classes_list_back = classes_list-1
+
+            print(boxes_list)
+            print(scores_list)
+            print(classes_list)
+            #######################################################################
             vis_util.visualize_boxes_and_labels_on_image_array(
                 image_np,
-                np.squeeze(boxes),
-                np.squeeze(classes).astype(np.int32),
-                np.squeeze(scores),
+                boxes_list,
+                classes_list_back,
+                scores_list,
                 category_index,
                 use_normalized_coordinates=True,
+                min_score_thresh=0.1,
                 line_thickness=8)
-            plt.imsave(os.path.join(FLAGS.output_dir, 'output.png'), image_np)
+            return image_np, category_index
+
+def py_cpu_nms(dets, scores, thresh):
+    """Pure Python NMS baseline."""
+    #x1、y1、x2、y2、以及score赋值
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    #每一个检测框的面积
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    # #按照score置信度降序排序
+    order = scores.argsort()[::-1]
+    keep = []
+    # #保留的结果框集合
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+        #保留该类剩余box中得分最高的一个 #得到相交区域,左上及右下
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+        #计算相交的面积,不重叠时面积为0
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        #计算IoU：重叠面积 /（面积1+面积2-重叠面积）
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+        #保留IoU小于阈值的box
+        inds = np.where(ovr <= thresh)[0]
+        order = order[inds + 1]
+        #因为ovr数组的长度比order数组少一个,所以这里要将所有下标后移一位
+    return keep
+
+
+if __name__ == '__main__':
+    start = time.time()
+    test_img_path = 'input_dir/00007.jpg'
+    np_image, category_index = mainfunction(test_img_path)
+    middle = time.time()
+    plt.imsave('output_dir/00007.png', np_image)
+    end = time.time()
+    print(middle - start)
+    print(end - middle)
